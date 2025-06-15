87e4ac922ac6d54f64b646b504e3fc1052dc90bb
PieroAGP <gppiero95@gmail.com>
2025-06-15 21:06:52 +0200

    cambio en app

--- src/app.js
+++ src/app.js
@@ -30,7 +30,29 @@ const corsOptions = {
 };
 //app.use(cors(corsOptions));
 
-app.use(cors());
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://tfmvetfrontpiero-production.up.railway.app',
+    'http://localhost:3000',
+    'http://localhost:5173'
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  next();
+});
 
 // Middleware adicional para manejar preflight requests manualmente si es necesario
 /*
